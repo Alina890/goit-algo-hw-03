92ae701127778d7825cd4a4021eb74459e9f66f4
Alina890 <avojtenko89@gmail.com>
2024-03-16 01:08:05 +0200

    Refined (4-1) hw

--- task_2.py
+++ task_2.py
@@ -3,21 +3,20 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     lottery_numbers = []
-    numbers_tickets = random.randrange(1,1000)
-    for numbers in lottery_numbers: 
-        if len(lottery_numbers) == quantity and min >= 1 and max <= 1000:
-            lottery_numbers.append(numbers_tickets)
-            continue
-        else:
-            lottery_numbers = []
-    return 
-    #while len(lottery_numbers) == quantity:
-        #numbers_tickets = random.randrange(1,1000)
-        #lottery_numbers.append(numbers_tickets)
-        #continue
-    #return (lottery_numbers)
+    for number in lottery_numbers: 
+        numbers_tickets = random.randrange(min,max)
+        try:
+            if len(lottery_numbers) <= quantity and number >= min and number <= max:
+                lottery_numbers.append(numbers_tickets)
+                continue
+            else:
+                lottery_numbers = []
+        except:
+            print ("Спробуйте ще")
+    return (lottery_numbers)
 
 min = 1
 max = 1000
 quantity = random.randint(1,1000)
-print (get_numbers_ticket(min, max, quantity))
+lottery_numbers = get_numbers_ticket(min, max, quantity)
+print("Ваші лотерейні числа:", lottery_numbers)
--- task_4(1).py
+++ task_4(1).py
@@ -2,15 +2,13 @@ from datetime import datetime, timedelta
 
 users = [
     {"name": "Alison", "birthday": "1994.08.06"},
-    {"name": "Deniel", "birthday": "1990.02.12"},
-    {"name": "Barbara", "birthday": "1989.04.23"},
+    {"name": "Deniel", "birthday": "1990.03.16"},
+    {"name": "Barbara", "birthday": "1989.03.17"},
     {"name": "Semuel", "birthday": "1996.11.03"},
     {"name": "Elisabeth", "birthday": "1999.06.05"}
 ]
 
 def find_next_weekday(day,weekday: int):
-    day = datetime.date()
-    weekday = datetime.weekday()
     days_ahead = weekday - day.weekday()
     if days_ahead <= 0:
         days_ahead += 7
@@ -23,23 +21,23 @@ def get_prepared_users(users):
             birthday_days = datetime.strptime(users["birthday"], "%Y.%m.%d").date()
             prepared_users.append({"name": user["name"], "birthday": birthday_days})
         except ValueError:
-            print(f"Некортектна дата народження для користувача {user["name"]}")
+            print(f"Некоректна дата народження для користувача {user["name"]}")
     return (prepared_users)
 
 def get_uncoming_birthdays(prepared_users):
     days = 7
     current_date = datetime.today().date()
-    birthday_days = datetime.strptime(users["birthday"], "%Y.%m.%d").date()
     uncoming_birthdays = []
     for user in prepared_users:
+        birthday_days = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
         birthday_this_year = birthday_days.replace(year=current_date.year)
         if birthday_this_year < current_date:
             birthday_this_year = birthday_this_year.replace(year=current_date.year + 1)
-            if 0 <= (birthday_this_year - current_date).days <= days:
-                if birthday_this_year.weekday() >= 5:
-                    birthday_this_year = find_next_weekday(birthday_this_year, 0)
-                congratulation_date_string = birthday_this_year.strftime("%Y,%m,%d")
-                uncoming_birthdays.append({"name": user["name"], "congratulation_date": congratulation_date_string})
+        if 0 <= (birthday_this_year - current_date).days <= days:
+            if birthday_this_year.weekday() >= 5:
+                birthday_this_year = find_next_weekday(birthday_this_year, 0)
+            congratulation_date_string = birthday_this_year.strftime("%Y,%m,%d")
+            uncoming_birthdays.append({"name": user["name"], "congratulation_date": congratulation_date_string})
     return (uncoming_birthdays) 
 
 print (get_uncoming_birthdays(users))
